33a11a195128a20b138d2aec4fae74ca69d59b1d
Reshika Shrestha <shresthareshika@gmail.com>
2021-04-02 20:50:27 +0545

    change in home

---
+++ src/Admin/AdminHeader.js
@@ -0,0 +1,43 @@
+// import React from 'react';
+// import '../Header.css';
+// import logo from '../logo.png';
+// import SearchIcon from '@material-ui/icons/Search';
+
+// import { Link } from 'react-router-dom';
+
+// function AdminHeader(){
+  
+//     return(
+//         <div className='header'>
+           
+//             <img className="header_logo" src={logo} alt="Logo"/>
+           
+                
+//                 <div className ="header_search">
+//                     <input className="header_searchInput" type="text"/>
+//                     <SearchIcon className="header_searchIcon"/>
+//                 </div>
+//                 <div className="header_nav">
+//                     <Link to ="/login">
+//                     <div className = 'header_option'>
+//                         <span className='header_optionLineOne'>Hello Customer</span>
+//                         <span className='header_optionLineTwo'>Sign In</span>
+
+//                     </div>
+//                     </Link>
+//                     <div className = 'header_option'>
+//                         <span className='header_optionLineOne'>Returns</span>
+//                         <span className='header_optionLineTwo'>& Orders</span>
+//                     </div>
+//                     <div className = 'header_option'>
+//                         <span className='header_optionLineOne'>Help</span>
+//                         <span className='header_optionLineTwo'>& Contact Us</span>
+//                     </div>
+                    
+                    
+
+//                 </div>
+//         </div>
+//     )
+// }
+// export default AdminHeader;
--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-
 import React from "react";
 import './App.css';
 import Header from "./Header";
@@ -9,37 +8,47 @@ import Checkout from "./Checkout";
 import Login from "./Login";
 import Register from './Register';
 import Payment from './Payment';
+import Dashboard from './Admin/Dashboard';
+import AdminHeader from './Admin/AdminHeader';
 
 function App() {
   return (
     <Router>
     <div className="App">
-    <Header/>
       <Switch>
         <Route path ="/login">
+        <Header/>
           <Login/>
         </Route>
 
         <Route path ="/register">
+        <Header/>
           <Register/>
         </Route>
 
           <Route path="/checkout">
+          <Header/>
             <Checkout/>
           </Route>
 
           <Route path="/payment">
+          <Header/>
             <Payment/>
           </Route>
 
           <Route path="/">
-        
+          <Header/>
             <Home/>
+          </Route>
+          <Route path="/admin">
+          <AdminHeader/>
+            <Dashboard/>
           </Route>
       </Switch>
       
     </div>
     </Router>
+    
   );
 }
 
