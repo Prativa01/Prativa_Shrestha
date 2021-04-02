8a186214571d103737a9a88504884985ad1577d9
Reshika Shrestha <shresthareshika@gmail.com>
2021-04-02 13:28:14 +0545

    home and its css

--- src/Home.js
+++ src/Home.js
@@ -1,71 +1,71 @@
-// import React from 'react';
-// import './Home.css';
-// import frozen from './frozen.jpg';
-// import Product from './Product';
-// import chajang from "./chajangNoodles.jpg";
-// import sausage from './sausage.jpg';
-// import Robert from './Robertsweet.png';
+import React from 'react';
+import './Home.css';
+import frozen from './frozen.jpg';
+import Product from './Product';
+import chajang from "./chajangNoodles.jpg";
+import sausage from './sausage.jpg';
+import Robert from './Robertsweet.png';
 
 
-// function Home(){
-//     return(
-//         <div className='home'>
-//             <div className="home_container">
-//             <img className="home_image" src={frozen} alt=""/>
+function Home(){
+    return(
+        <div className='home'>
+            <div className="home_container">
+            <img className="home_image" src={frozen} alt=""/>
             
-//             <div className="home_row">
-//                <Product 
-//                 id="1124"
-//                 title="Paldo Jjajang Men Chajang Noodles 
-//                     - 800 gm (200 gm x 4 packets)" 
-//                     price={150}
-//                      image={chajang} 
-//                      rating={4} />
-//                 <Product 
-//                 id="234"
-//                     title="Frozen Chicken Sausage" 
-//                     price={350}
-//                      image={sausage} 
-//                      rating={4}/>
-//             </div>
+            <div className="home_row">
+               <Product 
+                id="1124"
+                title="Paldo Jjajang Men Chajang Noodles 
+                    - 800 gm (200 gm x 4 packets)" 
+                    price={150}
+                     image={chajang} 
+                     rating={4} />
+                <Product 
+                id="234"
+                    title="Frozen Chicken Sausage" 
+                    price={350}
+                     image={sausage} 
+                     rating={4}/>
+            </div>
 
-//             <div className="home_row">
-//                 <Product
-//                     title="Robertson Winery Natural Sweet Red " 
-//                     price={450}
-//                     image={Robert} 
-//                     rating={5}/>
-//                 <Product
-//                     title="Paldo Jjajang Men Chajang Noodles 
-//                     - 800 gm (200 gm x 4 packets)" 
-//                     price={150}
-//                     image={chajang} 
-//                     rating={4}/>
-//                 <Product
-//                     title="Paldo Jjajang Men Chajang Noodles 
-//                     - 800 gm (200 gm x 4 packets)" 
-//                     price={150}
-//                     image={chajang} 
-//                     rating={4}/>
-//             </div>
+            <div className="home_row">
+                <Product
+                    title="Robertson Winery Natural Sweet Red " 
+                    price={450}
+                    image={Robert} 
+                    rating={5}/>
+                <Product
+                    title="Paldo Jjajang Men Chajang Noodles 
+                    - 800 gm (200 gm x 4 packets)" 
+                    price={150}
+                    image={chajang} 
+                    rating={4}/>
+                <Product
+                    title="Paldo Jjajang Men Chajang Noodles 
+                    - 800 gm (200 gm x 4 packets)" 
+                    price={150}
+                    image={chajang} 
+                    rating={4}/>
+            </div>
 
-//             <div className="home_row">
-//                 <Product
-//                     title="Paldo Jjajang Men Chajang Noodles 
-//                     - 800 gm (200 gm x 4 packets)" 
-//                     price={150}
-//                     image={chajang} 
-//                     rating={4}/>
-//                 <Product
-//                     title="Paldo Jjajang Men Chajang Noodles 
-//                     - 800 gm (200 gm x 4 packets)" 
-//                     price={150}
-//                     image={chajang} 
-//                     rating={4}/>
-//             </div>
+            <div className="home_row">
+                <Product
+                    title="Paldo Jjajang Men Chajang Noodles 
+                    - 800 gm (200 gm x 4 packets)" 
+                    price={150}
+                    image={chajang} 
+                    rating={4}/>
+                <Product
+                    title="Paldo Jjajang Men Chajang Noodles 
+                    - 800 gm (200 gm x 4 packets)" 
+                    price={150}
+                    image={chajang} 
+                    rating={4}/>
+            </div>
 
-//             </div>
-//         </div>
-//     )
-// }
-// export default Home;
+            </div>
+        </div>
+    )
+}
+export default Home;
