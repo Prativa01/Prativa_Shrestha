f884d36bce5690325dbe81358684de02021c79ed
Reshika Shrestha <shresthareshika@gmail.com>
2021-04-03 14:56:11 +0545

    checkout and its css

--- src/Checkout.js
+++ src/Checkout.js
@@ -1,42 +1,42 @@
-// import React, { Component } from 'react';
-// import './Checkout.css';
-// import ad from './ad.png';
-// import Subtotal from './Subtotal';
-// import { StateContext, useStateValue } from './StateProvider';
-// import CheckoutProduct from './CheckoutProduct';
+import React, { Component } from 'react';
+import './Checkout.css';
+import ad from './ad.png';
+import Subtotal from './Subtotal';
+import { StateContext, useStateValue } from './StateProvider';
+import CheckoutProduct from './CheckoutProduct';
 
-// function Checkout(){
+function Checkout(){
    
-//     const [{basket}, dispatch] = useStateValue();
+    const [{basket}, dispatch] = useStateValue();
 
-//     return(
-//         <div className="checkout">
-//           <div className="checkout_left">
-//               <img className="checkout_ad" src={ad} alt=""/>
+    return(
+        <div className="checkout">
+          <div className="checkout_left">
+              <img className="checkout_ad" src={ad} alt=""/>
 
-//               <div>
+              <div>
                   
-//                   <h2 className="checkout_title">Your shopping Basket</h2>
-//                   {basket.map(item =>(
-//                       <CheckoutProduct
-//                       id={item.id}
-//                       title={item.title}
-//                       image ={item.image}
-//                       price ={item.price}
-//                       rating ={item.rating}
-//                       >
+                  <h2 className="checkout_title">Your shopping Basket</h2>
+                  {basket.map(item =>(
+                      <CheckoutProduct
+                      id={item.id}
+                      title={item.title}
+                      image ={item.image}
+                      price ={item.price}
+                      rating ={item.rating}
+                      >
                           
-//                       </CheckoutProduct>
-//                   ))}
+                      </CheckoutProduct>
+                  ))}
 
-//               </div>
-//           </div>
-//           <div className="checkout_right">
-//               <Subtotal/>
+              </div>
+          </div>
+          <div className="checkout_right">
+              <Subtotal/>
              
-//           </div>
-//         </div>
-//     )
-// }
+          </div>
+        </div>
+    )
+}
 
-// export default Checkout;
+export default Checkout;
