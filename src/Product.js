94072fad528bcfceb0dfae164bd0e66d63e10069
Reshika Shrestha <shresthareshika@gmail.com>
2021-04-03 08:52:47 +0545

    product and it css

--- src/Product.js
+++ src/Product.js
@@ -1,46 +1,46 @@
-// import React from 'react';
-// import "./Product.css";
-// import chajang from "./chajangNoodles.jpg"
-// import { useStateValue } from './StateProvider';
+import React from 'react';
+import "./Product.css";
+import chajang from "./chajangNoodles.jpg"
+import { useStateValue } from './StateProvider';
 
-// function Product({id,title,image,price,rating}){
-//     const [{basket}, dispatch] = useStateValue();
-//     //dispatch is how we manipulate the data
+function Product({id,title,image,price,rating}){
+    const [{basket}, dispatch] = useStateValue();
+    //dispatch is how we manipulate the data
 
-//     console.log("this is the basket >>>", basket)
-//     const addToBasket =() =>{
-//         // dispatch the item into the data layer
-//         dispatch({
-//             type:'ADD_TO_BASKET',
-//             item:{
-//                 id:id,
-//                 title:title,
-//                 image:image,
-//                 price:price,
-//                 rating:rating
-//             }
-//         });
-//     };
-//     return(
-//         <div className="product">
-//             <div className="product_info">
-//                 <p>{title}</p>
-//                 <p className='product_price'>
-//                    <small>Price Rs </small>
-//                     <strong>{price}</strong>
-//                 </p>
-//                 <div className="product_rating">
-//                     {Array(rating).fill().map((_ , i) => (
-//                         <p>⭐</p>
-//                     ))}
+    console.log("this is the basket >>>", basket)
+    const addToBasket =() =>{
+        // dispatch the item into the data layer
+        dispatch({
+            type:'ADD_TO_BASKET',
+            item:{
+                id:id,
+                title:title,
+                image:image,
+                price:price,
+                rating:rating
+            }
+        });
+    };
+    return(
+        <div className="product">
+            <div className="product_info">
+                <p>{title}</p>
+                <p className='product_price'>
+                   <small>Price Rs </small>
+                    <strong>{price}</strong>
+                </p>
+                <div className="product_rating">
+                    {Array(rating).fill().map((_ , i) => (
+                        <p>⭐</p>
+                    ))}
                     
                     
-//                 </div>
-//             </div>
-//             <img src={image} alt=""/>
-//             <button onClick={addToBasket}>Add to Basket</button>
-//         </div>
-//     )
-// }
+                </div>
+            </div>
+            <img src={image} alt=""/>
+            <button onClick={addToBasket}>Add to Basket</button>
+        </div>
+    )
+}
 
-// export default Product;
+export default Product;
