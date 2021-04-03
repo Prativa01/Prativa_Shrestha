21e2a2463d5191aee43e6ff0c58750a1369ac4d0
Reshika Shrestha <shresthareshika@gmail.com>
2021-04-03 20:58:22 +0545

    State Provider

--- src/StateProvider.js
+++ src/StateProvider.js
@@ -1,14 +1,14 @@
-// import React,{state, createContext, useContext, useReducer} from 'react';
+import React,{state, createContext, useContext, useReducer} from 'react';
 
-// //Prepare the dataLayer
-// export const StateContext = createContext();
+//Prepare the dataLayer
+export const StateContext = createContext();
 
-// //Wrap our app and provide the data layer
-// export const StateProvider =({reducer,initialState, children})=>(
-//     <StateContext.Provider value={useReducer(reducer, initialState)}>
-//         {children}
-//     </StateContext.Provider>
-// );
+//Wrap our app and provide the data layer
+export const StateProvider =({reducer,initialState, children})=>(
+    <StateContext.Provider value={useReducer(reducer, initialState)}>
+        {children}
+    </StateContext.Provider>
+);
 
-// //Pull information from the data layout
-// export const useStateValue =()=>useContext(StateContext);
+//Pull information from the data layout
+export const useStateValue =()=>useContext(StateContext);
