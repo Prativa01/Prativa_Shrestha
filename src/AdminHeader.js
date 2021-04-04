6d0b59045ae5d3fe26aed2028060e5b3c14aebec
Reshika Shrestha <shresthareshika@gmail.com>
2021-04-04 15:29:49 +0545

    Admin Header

--- src/AdminHeader.js
+++ src/AdminHeader.js
@@ -1,48 +1,48 @@
-// import React from 'react';
-// import './Header.css';
-// import logo from './logo.png';
-// import { Link } from 'react-router-dom';
-// import SearchIcon from '@material-ui/icons/Search';
+import React from 'react';
+import './Header.css';
+import logo from './logo.png';
+import { Link } from 'react-router-dom';
+import SearchIcon from '@material-ui/icons/Search';
 
 
-// function AdminHeader(){
+function AdminHeader(){
 
-//     return(
-//         <div className='header'>
-//             <Link to ="/admin">
-//             <img className="header_logo" src={logo} alt="Logo"/>
-//            </Link>
+    return(
+        <div className='header'>
+            <Link to ="/admin">
+            <img className="header_logo" src={logo} alt="Logo"/>
+           </Link>
                 
-//                 <div className ="header_search">
-//                     <input className="header_searchInput" type="text"/>
-//                     <SearchIcon className="header_searchIcon"/>
-//                 </div>
-//                 <div className="header_nav">
+                <div className ="header_search">
+                    <input className="header_searchInput" type="text"/>
+                    <SearchIcon className="header_searchIcon"/>
+                </div>
+                <div className="header_nav">
                     
-//                     <div className = 'header_option'>
-//                         <span className='header_optionLineOne'>Hello Admin</span>
-//                         <span className='header_optionLineTwo'>WELCOME</span>
+                    <div className = 'header_option'>
+                        <span className='header_optionLineOne'>Hello Admin</span>
+                        <span className='header_optionLineTwo'>WELCOME</span>
 
-//                     </div>
+                    </div>
                     
-//                     <div className = 'header_option'>
-//                         <span className='header_optionLineOne'>Cart</span>
-//                         <span className='header_optionLineTwo'>& Orders</span>
-//                     </div>
-//                     <div className = 'header_option'>
-//                         <span className='header_optionLineOne'>Help</span>
-//                         <span className='header_optionLineTwo'>& Feedback</span>
-//                     </div>
-//                     <Link to ="/addproduct">
-//                     <div className = 'header_option'>
-//                         <span className='header_optionLineOne'>Add Product</span>
-//                         <span className='header_optionLineTwo'> +</span>
-//                     </div>
-//                     </Link>
+                    <div className = 'header_option'>
+                        <span className='header_optionLineOne'>Cart</span>
+                        <span className='header_optionLineTwo'>& Orders</span>
+                    </div>
+                    <div className = 'header_option'>
+                        <span className='header_optionLineOne'>Help</span>
+                        <span className='header_optionLineTwo'>& Feedback</span>
+                    </div>
+                    <Link to ="/addproduct">
+                    <div className = 'header_option'>
+                        <span className='header_optionLineOne'>Add Product</span>
+                        <span className='header_optionLineTwo'> +</span>
+                    </div>
+                    </Link>
                     
 
-//                 </div>
-//         </div>
-//     )
-// }
-// export default AdminHeader;
+                </div>
+        </div>
+    )
+}
+export default AdminHeader;
