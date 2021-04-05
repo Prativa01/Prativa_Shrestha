701f2eb8be6e9ae2a738ecf0f564f40fe92cfc39
Reshika Shrestha <shresthareshika@gmail.com>
2021-04-05 21:17:35 +0545

    change in About design

--- src/About.js
+++ src/About.js
@@ -1,55 +1,44 @@
-import React from 'react';
-function About(){
-    return (
-        <div class="main-content">
-            <section class="section">
-                <div class="section-body">
-                    <div class="row">
-                        <div class="col-12">
-                            <div class="card">
-                                <div class="card-header">
-                                    <h4>Simple Summernote</h4>
-                                </div>
-                                <div class="card-body">
-                                        <div class="form-group row mb-4">
-                                        <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3">Title</label>
-                                        <div class="col-sm-12 col-md-7">
-                                            <input type="text" class="form-control"/>
-                                        
-                                        </div>
-                                        </div>
-                                        <div class="form-group row mb-4">
-                                        <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3">Category</label>
-                                        <div class="col-sm-12 col-md-7">
-                                            <select class="form-control selectric">
-                                            <option>Tech</option>
-                                            <option>News</option>
-                                            <option>Political</option>
-                                            </select>
-                                        </div>
-                                        </div>
-                                        <div class="form-group row mb-4">
-                                        <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3">Content</label>
-                                        <div class="col-sm-12 col-md-7">
-                                            <textarea class="summernote-simple"></textarea>
-                                        </div>
-                                        </div>
-                                        <div class="form-group row mb-4">
-                                        <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3"></label>
-                                        <div class="col-sm-12 col-md-7">
-                                            <button class="btn btn-primary">Publish</button>
-                                        </div>
-                                        </div>
-                                    </div>
-                                    
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                
-            </section>
-        </div>
-    )
 
+import React, { Component } from'react';
+import frozen from './frozen.jpg';
+import './About.css';
+
+class About extends Component{
+    render(){
+    return (
+        <div className="ac">
+        <div className="about">
+         <div className="container" data-aos="fade-up">
+
+                <div className="row">
+                    <h3 className="us">About Us</h3>
+                <div className="col-lg-6 order-1 order-lg-2" data-aos="fade-left" data-aos-delay="100">
+                    <img src={frozen} className="img-fluid" alt=""/>
+                </div>
+                <div className="col-lg-6 pt-4 pt-lg-0 order-2 order-lg-1 content" data-aos="fade-right" data-aos-delay="100" >
+                    <h3>Foodmart</h3>
+                    <p className="fst-italic">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+                    magna aliqua.
+                    </p>
+                    <ul>
+                    <li><i className="ri-check-double-line"></i> Ullamco laboris nisi ut aliquip ex ea commodo consequat.</li>
+                    <li><i className="ri-check-double-line"></i> Duis aute irure dolor in reprehenderit in voluptate velit.</li>
+                    <li><i className="ri-check-double-line"></i> Ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate trideta storacalaperda mastiro dolore eu fugiat nulla pariatur.</li>
+                    </ul>
+                    <p>
+                    Ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+                    velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident
+                    </p>
+                </div>
+                </div>
+                </div>
+         </div>   
+  
+                 
+</div>
+      
+    )
+        }
 }
 export default About;
